661842c81b85b9dbd40ecf502ae84a4528e22516
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-03-23 17:21:30 -0500

    refactorizando

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,35 @@
-function fichaDatos() {
+window.addEventListener('load', function(){
+  var button = document.getElementById('button');
+  button.addEventListener('click', function(){
+    var nombre= document.getElementById('firstname').value;
+    var apellido= document.getElementById('lastname').value;
+    var dni= document.getElementById('dni').value;
+    var direccion= document.getElementById('address').value;
+    var soloLetra= /[a-zA-Z]/;
+    var soloNumeros= /[0123456789]{8}/;
+    //validando espacios vacios
+    if(nombre =! "" && apellido != "" && dni != "" && direccion =! ""){
+    //validando solo letras en nombre y apellido
+      if(soloLetra.test(nombre) != true && soloLetra.test(apellido) != true){
+        alert("Completa los campos con datos validos");
+      }
+      if(soloNumeros.test(dni) != true ){
+        alert("Completa los campos con datos validas")
+      }
+    }else{
+      alert("Completa todos los datos");
+    }
+  })
+})
+/*
+var str = "123456789";
+var reg = /[a-zA-Z]/g;
+if(reg.test(str) != true){
+console.log("Error")
+}
+
+*/
+/*function fichaDatos() {
  var nombre= document.getElementById('firstname').value;
   var apellido= document.getElementById('lastname').value;
   var dni= document.getElementById('dni').value;
@@ -9,4 +40,4 @@ if(nombre!="" && apellido!="" && dni!="" && direccion!=""){
   + nombre + "<br><br> Apellido: " + apellido + "<br><br> DNI: "
   + dni + "<br><br> Direccion: " + direccion);
  };
-};
+};*/
